503cb3d788b2c753b413758f5be002fefc5010a2
Hugo van Schalkwyk <hugovans2000@gmail.com>
2024-07-30 13:37:27 +0200

    style: 💄 Improved not found component

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,15 @@
+import { FiHome } from 'solid-icons/fi';
+
 export default function NotFound() {
   return (
-    <div>
-      <h1>Not Found. Return Home</h1>
-      <a href="/home">Home</a>
+    <div class="flex flex-col items-center gap-4 p-6 text-center">
+      <h1 class="max-w-96 text-lg">
+        The page you are looking for does not exist. Please check the url or
+        return home.
+      </h1>
+      <a href="/home" class="btn btn-ghost bg-neutral">
+        <FiHome /> Home
+      </a>
     </div>
   );
 }
