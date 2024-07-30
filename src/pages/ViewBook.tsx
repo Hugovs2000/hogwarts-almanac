50bc5101b5a1062f96988e859035c1d6883fb7e6
Hugo van Schalkwyk <hugovans2000@gmail.com>
2024-07-30 13:18:44 +0200

    fix: 🐛 Removed the Suspense tag and replaced it with a switch statement

--- src/pages/ViewBook.tsx
+++ src/pages/ViewBook.tsx
@@ -1,42 +1,42 @@
 import { useParams } from '@solidjs/router';
 import { createQuery } from '@tanstack/solid-query';
-import { Show, Suspense, createSignal } from 'solid-js';
-import { Book } from '../models/books';
+import { Match, Switch } from 'solid-js';
 import { getBooks } from '../services/api.service';
 
 export default function ViewBook() {
   const params = useParams();
-  const [selectedBook, setSelectedBook] = createSignal<Book | null>(null);
 
   const query = createQuery(() => ({
     queryKey: ['getBook'],
     queryFn: () => getBooks(),
     select: books => {
-      const book = books.find(book => book.number.toString() === params.id);
-      if (book) {
-        setSelectedBook(book);
-      }
+      return books.find(book => book.number.toString() === params.id);
     },
   }));
 
   return (
-    <Suspense>
-      <Show when={query.isPending}>Loading...</Show>
-      <Show when={!query.isPending && !selectedBook()}>No book was found</Show>
-      <Show when={selectedBook()}>
-        <div class="flex w-full flex-col items-center gap-2 p-6">
-          <img
-            src={selectedBook()?.cover}
-            class="max-w-full object-contain object-center"
-          />
-          <h2 class="text-xl">{selectedBook()?.title}</h2>
-          <div class="flex gap-4">
-            <p>{selectedBook()?.releaseDate}</p>
-            <p>{selectedBook()?.pages} pages</p>
+    <Switch>
+      <Match when={query.isLoading}>Loading...</Match>
+      <Match when={query.data === undefined || query.error}>
+        Apologies, there was an error. Could not find the book you are looking
+        for.
+      </Match>
+      <Match when={query.data}>
+        {query.data && (
+          <div class="flex w-full flex-col items-center gap-2 p-6">
+            <img
+              src={query.data.cover}
+              class="max-w-full object-contain object-center"
+            />
+            <h2 class="text-xl">{query.data.title}</h2>
+            <div class="flex gap-4">
+              <p>{query.data.releaseDate}</p>
+              <p>{query.data.pages} pages</p>
+            </div>
+            <p class="text-center">{query.data.description}</p>
           </div>
-          <p class="text-center">{selectedBook()?.description}</p>
-        </div>
-      </Show>
-    </Suspense>
+        )}
+      </Match>
+    </Switch>
   );
 }
--- src/pages/ViewCharacter.tsx
+++ src/pages/ViewCharacter.tsx
@@ -1,46 +1,43 @@
 import { useParams } from '@solidjs/router';
 import { createQuery } from '@tanstack/solid-query';
-import { Show, Suspense, createSignal } from 'solid-js';
-import { Character } from '../models/characters';
+import { Match, Switch } from 'solid-js';
 import { getCharacters } from '../services/api.service';
 
 export default function ViewCharacter() {
   const params = useParams();
-  const [selectedCharacter, setSelectedCharacter] =
-    createSignal<Character | null>(null);
 
   const query = createQuery(() => ({
     queryKey: ['getCharacter'],
     queryFn: () => getCharacters(),
     select: characters => {
-      const character = characters.find(
+      return characters.find(
         character => character.nickname.toString() === params.name
       );
-      if (character) {
-        setSelectedCharacter(character);
-      }
     },
   }));
 
   return (
-    <Suspense>
-      <Show when={query.isPending}>Loading...</Show>
-      <Show when={!query.isPending && !selectedCharacter()}>
-        No character was found
-      </Show>
-      <Show when={selectedCharacter()}>
-        <div class="flex w-full flex-col items-center gap-2 p-6">
-          <img
-            src={selectedCharacter()?.image}
-            class="max-w-full object-contain object-center"
-          />
-          <h2 class="text-xl">{selectedCharacter()?.fullName}</h2>
-          <div class="flex gap-4">
-            <p>{selectedCharacter()?.birthdate}</p>
-            <p>{selectedCharacter()?.hogwartsHouse}</p>
+    <Switch>
+      <Match when={query.isLoading}>Loading...</Match>
+      <Match when={query.data === undefined || query.error}>
+        Apologies, there was an error. Could not find the character you are
+        looking for.
+      </Match>
+      <Match when={query.data}>
+        {query.data && (
+          <div class="flex w-full flex-col items-center gap-2 p-6">
+            <img
+              src={query.data.image}
+              class="max-w-full object-contain object-center"
+            />
+            <h2 class="text-xl">{query.data.fullName}</h2>
+            <div class="flex gap-4">
+              <p>{query.data.birthdate}</p>
+              <p>{query.data.hogwartsHouse}</p>
+            </div>
           </div>
-        </div>
-      </Show>
-    </Suspense>
+        )}
+      </Match>
+    </Switch>
   );
 }
