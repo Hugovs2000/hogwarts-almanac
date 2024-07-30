44313faa886373413ecf1add1a961a612d8efb23
Hugo van Schalkwyk <hugovans2000@gmail.com>
2024-07-30 20:19:46 +0200

    style: 💄 Improved the drawer

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -9,7 +9,12 @@ export default function Drawer() {
         for="drawer-left"
         aria-label="close sidebar"
         class="drawer-overlay"></label>
-      <ul class="menu min-h-full w-80 bg-base-200 p-4">
+      <ul class="menu min-h-full w-80 gap-4 bg-base-200 p-4 text-lg">
+        <li>
+          <div class="mx-2 flex-1 px-2">
+            <a href="/home">Hogwarts Almanac</a>
+          </div>
+        </li>
         <li>
           <a href="/home">
             <FiHome />
