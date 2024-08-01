d8ac2a8c1a37d9905760c04c292eb3195b914c4a
Hugo van Schalkwyk <hugovans2000@gmail.com>
2024-08-01 16:57:01 +0200

    feat: ✨ Finished quiz by showing the score

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,12 +1,40 @@
+import { createForm } from '@tanstack/solid-form';
 import { createQuery } from '@tanstack/solid-query';
-import { For, createSignal } from 'solid-js';
+import { zodValidator } from '@tanstack/zod-form-adapter';
+import { For, Setter, createSignal } from 'solid-js';
+import { z } from 'zod';
 import { Spell } from '../models/spell';
 import { getSpells } from '../services/api.service';
 
-export default function QuestionCard({ question }: { question: Spell }) {
+export default function QuestionCard({
+  question,
+  questionNumber,
+  setQuestionIndex,
+  setScore,
+}: {
+  question: Spell;
+  questionNumber: number;
+  setQuestionIndex: Setter<number>;
+  setScore: Setter<number>;
+}) {
   const [randomSpellNames, setRandomSpellNames] = createSignal<string[] | null>(
     null
   );
+
+  const form = createForm(() => ({
+    validatorAdapter: zodValidator(),
+    defaultValues: {
+      selectedSpell: '',
+    },
+    onSubmit: async ({ value }) => {
+      if (value.selectedSpell === question.spell) {
+        setScore(prev => prev + 1);
+        setQuestionIndex(prev => prev + 1);
+      } else {
+        setQuestionIndex(prev => prev + 1);
+      }
+    },
+  }));
 
   createQuery(() => ({
     queryKey: ['getRandomSpellNames'],
@@ -17,17 +45,16 @@ export default function QuestionCard({ question }: { question: Spell }) {
         randomSpells.push(question.spell);
         let counter = 0;
         while (randomSpells.length < 4) {
+          const randomSpell =
+            spells[Math.floor(Math.random() * spells.length)].spell;
           if (
-            spells[counter].spell !== question.spell &&
-            !randomSpells.includes(spells[counter].spell)
+            randomSpell !== question.spell &&
+            !randomSpells.includes(randomSpell)
           ) {
-            randomSpells.push(
-              spells[Math.floor(Math.random() * spells.length)].spell
-            );
+            randomSpells.push(randomSpell);
           }
           counter++;
         }
-
         setRandomSpellNames(shuffle(randomSpells));
       }
     },
@@ -42,19 +69,48 @@ export default function QuestionCard({ question }: { question: Spell }) {
   };
 
   return (
-    <div class="flex w-full flex-col items-center gap-2 rounded-lg bg-neutral p-6 sm:w-96">
-      <h2 class="text-xl">{question.use}</h2>
+    <div class="flex min-h-64 w-full flex-col items-center gap-2 rounded-lg bg-neutral p-6 sm:w-96">
+      <h2 class="self-start text-xl">
+        {questionNumber}. {question.use}
+      </h2>
       <div class="divider m-0"></div>
-      <div class="w-full self-start pl-4">
-        <For each={randomSpellNames()}>
-          {spell => (
-            <div>
-              <input type="radio" id={spell} value={spell} class="mr-2" />
-              <label for={spell}>{spell}</label>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          e.stopPropagation();
+          form.handleSubmit();
+        }}
+        class="w-full">
+        <form.Field
+          name="selectedSpell"
+          validatorAdapter={zodValidator()}
+          validators={{
+            onChange: z.string().min(1),
+          }}
+          children={field => (
+            <div class="min-h-24 self-start pl-4">
+              <For each={randomSpellNames()}>
+                {spell => (
+                  <div>
+                    <input
+                      type="radio"
+                      name={field().name}
+                      id={spell}
+                      value={spell}
+                      onChange={() => field().setValue(spell)}
+                      class="mr-2"
+                    />
+                    <label for={spell}>{spell}</label>
+                  </div>
+                )}
+              </For>
             </div>
           )}
-        </For>
-      </div>
+        />
+        <button type="submit" class="mt-4">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -8,6 +8,7 @@ export default function Quiz() {
   const [spells, setSpells] = createSignal<Spells | null>(null);
   const [questions, setQuestions] = createSignal<Spells | null>(null);
   const [questionIndex, setQuestionIndex] = createSignal(0);
+  const [score, setScore] = createSignal(0);
 
   createQuery(() => ({
     queryKey: ['getSpells'],
@@ -24,12 +25,14 @@ export default function Quiz() {
     const currentSpells = spells();
     const questions: Spells = [];
     if (currentSpells && currentSpells.length > 0 && questions.length === 0) {
-      for (let i = 0; i < 10; i++) {
+      let counter = 0;
+      while (questions.length < 10) {
         const question =
           currentSpells[Math.floor(Math.random() * currentSpells.length)];
         if (!questions.includes(question)) {
           questions.push(question);
         }
+        counter++;
       }
       setQuestions(questions);
     }
@@ -42,16 +45,32 @@ export default function Quiz() {
 
   return (
     <div class="flex flex-col items-center gap-4 p-6">
+      <h1 class="text-3xl font-bold">Spells Quiz</h1>
       <For each={questions()}>
-        {question => (
+        {(question, index) => (
           <Show when={questionIndex() === questions()?.indexOf(question)}>
-            <QuestionCard question={question} />
+            <QuestionCard
+              question={question}
+              questionNumber={index() + 1} // Convert index to a number
+              setQuestionIndex={setQuestionIndex}
+              setScore={setScore}
+            />
           </Show>
         )}
       </For>
-      <Show when={questionIndex() <= 9}>
-        <button onClick={() => setQuestionIndex(prev => prev + 1)}>
-          Next Question
+      <Show when={questionIndex() < (questions()?.length ?? 0)}>
+        <h2 class="self-start text-xl font-semibold">Score: {score()}</h2>
+      </Show>
+      <Show when={questionIndex() === questions()?.length}>
+        <h2 class="text-xl font-semibold">Quiz complete!</h2>
+        <h3 class="text-lg">Your score: {score()}</h3>
+        <button
+          class="btn"
+          onClick={() => {
+            setQuestionIndex(0);
+            setScore(0);
+          }}>
+          Restart
         </button>
       </Show>
     </div>
